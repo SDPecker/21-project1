e6cb20e031f49a600d7e5a4675294f03eda7be23
SDPecker <bulexey@mail.ru>
2021-05-20 17:34:29 +0300

    Add files via upload

--- ft_memchr.c
+++ ft_memchr.c
@@ -2,14 +2,16 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	size_t	i;
-	char	*r;
+	size_t			i;
+	unsigned char	cn;
+	unsigned char	*r;
 
 	i = 0;
-	r = (char *)s;
+	r = (unsigned char *)s;
+	cn = (unsigned char)c;
 	while (i < n)
 	{
-		if (r[i] == c)
+		if (r[i] == cn)
 			return (&r[i]);
 		i++;
 	}
